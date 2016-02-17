1ad6f57f9e3e1608bd694efdf88807d0148a651f
Latimer <latibe@PEROOT.COM>
2016-02-17 18:29:08 +0000

    Adding your first angular route

--- DemoApp/app/js/app.js
+++ DemoApp/app/js/app.js
@@ -1,3 +1,9 @@
 'use strict';
 
-var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource']);
+var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource', 'ngRoute'])
+  .config(function($routeProvider) {
+    $routeProvider.when('/newEvent', {
+      templateUrl: 'templates/NewEvent.html',
+      controller: 'EditEventController'
+    })
+  });
